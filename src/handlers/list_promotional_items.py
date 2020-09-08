4661b5732170091d50ccbc962af6942b46161ab5
Haruna Oka <oka.haruna@classmethod.jp>
2020-09-08 20:04:49 +0900

    fix: ポート修正

--- src/handlers/list_promotional_items.py
+++ src/handlers/list_promotional_items.py
@@ -1,16 +1,31 @@
 import json
 import logging
 import os
+import psycopg2
+
 
 log_level = 'DEBUG' if os.environ['ENV'] == 'dev' else 'INFO'
 
 logger = logging.getLogger()
 logger.setLevel(log_level)
 
+# conn = psycopg2.connect(
+#     port=3306,
+#     host='sample-serverless-api-dbcluster-1u3b1x3nsni03.cluster-cvvtvtym0wbx.ap-northeast-1.rds.amazonaws.com',
+#     dbname="postgres",
+#     user="postgres",
+#     password="postgres")
+# cur = conn.cursor()
+# cur.execute(
+#     "CREATE TABLE promotional_items (id serial PRIMARY KEY, name varchar, category varchar);")
+# cur.close()
+# conn.close()
+
 
 def listPromotionalItems(limit):
     logging.debug(limit)
     # TODO db connection
+
     data = [
         {'promotional_item_id': '0001',
          'promotional_item_name': 'サンプル品1', 'category': '販促物'},
@@ -29,6 +44,7 @@ def listPromotionalItems(limit):
 def handler(event, context):
     try:
         logging.info(event)
+        logging.info(context)
 
         query_string_params = event.get('queryStringParameters')
         limit = query_string_params['limit'] if (query_string_params and query_string_params.get(
