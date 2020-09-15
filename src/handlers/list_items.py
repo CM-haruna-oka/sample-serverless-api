c1ef9e3aaa222bea81d6cfd125aeb6fd641e18d8
Haruna Oka <oka.haruna@classmethod.jp>
2020-09-15 11:56:52 +0900

    limitを追加

--- src/handlers/list_items.py
+++ src/handlers/list_items.py
@@ -24,15 +24,25 @@ logger.setLevel(log_level)
 DEFAULT_DATA_LIMIT = int(os.getenv('DEFAULT_DATA_LIMIT'))  # ページングのデフォルトかつ最大値
 
 
-def list_promotional_items(limit):
+def list_promotional_items(limit, last_key=None):
     logging.debug(limit)
     dynamodb = boto3.resource('dynamodb')
     TABLE_NAME = 'Items'
     table = dynamodb.Table(TABLE_NAME)
 
-    res = table.scan(TableName=TABLE_NAME, ConsistentRead=True)
-    logging.info(res)
-    return res['Items']
+    scan_kwargs = {
+        'ConsistentRead': True,
+        'Limit': limit
+    }
+
+    if last_key:
+        scan_kwargs['ExclusiveStartKey'] = last_key
+
+    response = table.scan(**scan_kwargs)
+    logging.info(response)
+    result = response.get('Items', [])
+
+    return result
 
 
 def validator_params(query):
@@ -59,7 +69,8 @@ def handler(event, context):
         query = event.get('queryStringParameters')
         params = validator_params(query)
 
-        result = list_promotional_items(params['limit'])
+        result = list_promotional_items(
+            params['limit'], event.get('last_evaluated_key'))
         logging.debug(result)
         return {
             'statusCode': 200,
