e2e8b58ca4129dc3f6c99410d5ab9bc10bc25331
Haruna Oka <oka.haruna@classmethod.jp>
2020-09-15 14:29:00 +0900

    不要な処理削除

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,3 @@ import pytest
 
 os.environ['ENV'] = 'dev'
 os.environ['DEFAULT_DATA_LIMIT'] = '20'
-
-
-@pytest.fixture(scope='session')
-def aws_credentials():
-    """Mocked AWS Credentials for moto."""
-    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
-    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-    os.environ['AWS_SESSION_TOKEN'] = 'testing'
