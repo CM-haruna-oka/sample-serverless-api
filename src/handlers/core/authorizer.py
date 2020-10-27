c1b1aa0ead8525a5a3231821f81a15d1b3f68392
Haruna Oka <oka.haruna@classmethod.jp>
2020-10-27 10:13:35 +0900

    refactor: エラーハンドリングの処理追加

--- src/handlers/core/authorizer.py
+++ src/handlers/core/authorizer.py
@@ -5,19 +5,17 @@ from core.auth import Auth
 
 def handler(event, context):
     utils.logging_settings()
+    logging.info(event)
+    auth = Auth()
+    token = auth.get_token(event)  # eventからトークン取得
+    if token is None:
+        logging.error('No token.')
+        result = auth.generate_policy('sample', 'Deny', '*')
+        return result
+    logging.info(token)
+
     try:
-        logging.info(event)
-
-        auth = Auth()
-        token = auth.get_token(event)  # eventからトークン取得
-
-        if token is None:
-            logging.error('No token.')
-            result = auth.generate_policy('sample', 'Deny', '*')
-            return result
-        logging.info(token)
-
-        resource = event['methodArn']
+        resource = event['methodArn']  # TODO
 
         # トークン検証
         result = auth.verify(token, resource)
