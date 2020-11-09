0bfdaa417267000bf74020cfd2835ff212553b27
Haruna Oka <oka.haruna@classmethod.jp>
2020-11-09 19:42:34 +0900

    refactor: サンプル関数を追加

--- src/layers/share/python/infrastructure/items.py
+++ src/layers/share/python/infrastructure/items.py
@@ -1,7 +1,22 @@
 import boto3
+from typing import List, Dict, Union
 
 
-def list_items(limit, last_key=None):
+def list_items(limit, last_key=None) -> List:
+    """商品一覧をDBから取得する
+
+    Parameters
+    ----------
+    limit : int
+        [description]
+    last_key : str, optional
+        [description], by default None
+
+    Returns
+    -------
+    List
+        [description]
+    """
     dynamodb = boto3.resource('dynamodb')
     TABLE_NAME = 'Items'
     table = dynamodb.Table(TABLE_NAME)
@@ -15,6 +30,74 @@ def list_items(limit, last_key=None):
         scan_kwargs['ExclusiveStartKey'] = last_key
 
     response = table.scan(**scan_kwargs)
-    result = {'items': response.get('Items', [])}
 
-    return result
+    return response.get('Items', [])
+
+
+def get_item(item_id: str) -> Dict:
+    """商品を一件取得する
+
+    Parameters
+    ----------
+    item_id : str
+        商品ID
+
+    Returns
+    -------
+    Dict
+        商品情報
+    """
+    # TODO
+    item = {
+        'item_id': 'item_0001',
+        'item_name': 'サンプル品1',
+        'category': '販促物'
+    }
+
+    return item
+
+
+def add_item(item: Dict) -> Dict:
+    """商品を一件追加する
+
+    Parameters
+    ----------
+    item : Dict
+        商品情報
+
+    Returns
+    -------
+    Dict
+        商品情報
+    """
+    # TODO
+    item = {
+        'item_id': 'item_0001',
+        'item_name': 'サンプル品1',
+        'category': '販促物'
+    }
+
+    return item
+
+
+def drop_item(item_id: str) -> Dict:
+    """商品を一件削除する
+
+    Parameters
+    ----------
+    item_id : str
+        商品ID
+
+    Returns
+    -------
+    Dict
+        商品情報
+    """
+    # TODO
+    item = {
+        'item_id': 'item_0001',
+        'item_name': 'サンプル品1',
+        'category': '販促物'
+    }
+
+    return item
