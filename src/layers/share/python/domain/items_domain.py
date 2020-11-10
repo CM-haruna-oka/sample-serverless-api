ba6522c18bc6e5a9906bbd6e3e7249f5f12ad3d4
Haruna Oka <oka.haruna@classmethod.jp>
2020-11-10 12:26:02 +0900

    refactor: repositoryに変更

--- src/layers/share/python/domain/items_domain.py
+++ src/layers/share/python/domain/items_domain.py
@@ -1,4 +1,4 @@
-from infrastructure import items
+from repository import items_repository
 import os
 from typing import Union, Any, Dict, List
 
@@ -6,35 +6,35 @@ from typing import Union, Any, Dict, List
 DEFAULT_DATA_LIMIT = int(os.getenv('DEFAULT_DATA_LIMIT'))
 
 
-def list_items(limit=DEFAULT_DATA_LIMIT, offset=None) -> List:
-    """商品一覧を取得
+class ItemService():
+    def list(self, limit=DEFAULT_DATA_LIMIT, offset=None) -> List:
+        """商品一覧を取得
 
-    Parameters
-    ----------
-    limit : int, optional
-        取得する件数, by default env DEFAULT_DATA_LIMIT
-    offset : str, optional
-        Acquisition start position, by default None
+        Parameters
+        ----------
+        limit : int, optional
+            取得する件数, by default env DEFAULT_DATA_LIMIT
+        offset : str, optional
+            Acquisition start position, by default None
 
-    Returns
-    -------
-    List
-        商品一覧
-    """
-    return items.list_items(limit, offset)
+        Returns
+        -------
+        List
+            商品一覧
+        """
+        return items_repository.list_items(limit, offset)
 
+    def get(self, item_id: str) -> Any:
+        """[summary]
 
-def get_item(item_id: str) -> Any:
-    """[summary]
+        Parameters
+        ----------
+        item_id : str
+            商品ID
 
-    Parameters
-    ----------
-    item_id : str
-        商品ID
-
-    Returns
-    -------
-    Any
-        商品情報
-    """
-    return items.get_item(item_id)
+        Returns
+        -------
+        Any
+            商品情報
+        """
+        return items_repository.get_item(item_id)
