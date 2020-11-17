5f6807e34b54349ee87783139eeca20d7d19588b
Haruna Oka <oka.haruna@classmethod.jp>
2020-11-17 13:40:46 +0900

    refactor: eventを型変数に変更

--- src/handlers/get_item.py
+++ src/handlers/get_item.py
@@ -1,15 +1,16 @@
 from domain.items_domain import ItemService
 from interface.handler import LambdaProxyHandler
 from interface.error import ValidationError, EntityNotFound
-from typing import Union, Any, Dict
+from typing import Any, Dict, TypeVar
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
 logger = Logger()
+T = TypeVar('T', APIGatewayProxyEvent, Dict[str, Any])
 
 
 class GetItemHandler(LambdaProxyHandler):
-    def handle(self, params):
+    def handle(self, params: Dict):
         logger.info(params)
         if 'item_id' not in params:
             raise ValidationError('item_id is required.')
@@ -24,7 +25,6 @@ class GetItemHandler(LambdaProxyHandler):
 
 
 @logger.inject_lambda_context(log_event=True)
-def handler(event: Union[APIGatewayProxyEvent, Dict[str, Any]],
-            context: LambdaContext) -> Dict[str, Any]:
+def handler(event: T, context: LambdaContext) -> Dict[str, Any]:
     get_item_handler = GetItemHandler(event)
     return get_item_handler.invoke(context, event)
